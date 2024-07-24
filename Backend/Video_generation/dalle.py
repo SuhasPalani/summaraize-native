821a595309a64b278a525fe7d2ced4b9d4ca0056
Harshith <harshithdr10@gmail.com>
2024-07-24 13:45:50 -0500

    dalle update

---
+++ Backend/Video_generation/dalle.py
@@ -0,0 +1,28 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.environ['CHATGPT_API_KEY']
+
+def generate_image(prompt, n=1, size="1024x1024"):
+    try:
+        response = openai.Image.create(
+            model="dall-e-3",  # Specify the model version
+            prompt=prompt,
+            n=n,
+            size=size
+        )
+        images = response.get('data')
+        for i, image in enumerate(images):
+            # This will print the URL of the generated image
+            print(f"Image {i+1}: {image['url']}")
+        return image['url']
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("trying again.................")
+        generate_image(prompt)
+
+if __name__ == '__main__':
+# Example usage
+    generate_image("boy standing in a beach")
