ee87c3f9977f3a47088dd7775c9961754a2d69ce
Sachin Shivanna <sachin.shivanna01@gmail.com>
2024-07-30 00:06:19 -0700

    bot history

--- Frontend/flask-backend/app.py
+++ Frontend/flask-backend/app.py
@@ -1,16 +1,80 @@
-from flask import Flask, jsonify, request
+import os
 import time
+import bcrypt
+import jwt
 import random
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from flask import Flask, request, jsonify, session, send_from_directory
+from bson.objectid import ObjectId
 from flask_cors import CORS
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 from db_actions.functions import *
 from retrievers.functions import *
-
+from user_auth_actions.functions import *
+from db_actions.video_handler import *
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-# Temporary in-memory storage for user interests
-user_interests_db = {}
+bot_schema = set_bot_schema()
+
+# Define the base directory and watch directory
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+WATCH_DIR = os.path.join(BASE_DIR, 'Frontend', 'flask-backend', 'assets')
+
+observer = start_observer(videos_collection, BASE_DIR, WATCH_DIR)            
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    user = user_auth.find_one({'username': username})
+    
+    if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
+        token = generate_token(user['_id'], app)
+        
+        session['token'] = token
+        
+        return jsonify({'status': 'success', 'token': token, 'user_id': str(user['_id'])}), 200
+    else:
+        return jsonify({'status': 'failure', 'message': 'Invalid credentials'}), 401
+
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+    
+    user_id = create_user(db, username, password)
+    
+    if user_id:
+        token = generate_token(user_id, app)
+        
+        return jsonify({"status": "success", "token": token, "message": "User created successfully", 'user_id': str(user_id)}), 201
+    else:
+        return jsonify({"status": "failure", "message": "User already exists"}), 400
+
+@app.route('/api/interest', methods=['POST'])
+def interest():
+    headers = request.headers
+    
+    isValid,clean_token, response_message = verify_user(headers,app)
+
+    if(not isValid):
+        return response_message
+    
+    user_id = response_message
+    data = request.json
+    interests_list = data.get('topics', [])
+
+    return update_interest(interests,user_id,interests_list)
+
 
 @app.route('/api/summary', methods=['GET'])
 def get_summary():
@@ -21,38 +85,58 @@ def get_summary():
     }
     return jsonify(summary_data)
 
-@app.route('/api/interest', methods=['POST'])
-def post_interest():
-    data = request.json
-    print("Received JSON data:", data)  # Debug statement
-    topics = data.get('topics', [])
-    print("Extracted topics:", topics)  # Debug statement
-    response = {
-        "status": "success",
-        "message": "Topics received!",
-        "received_data": topics
-    }
-    return jsonify(response)
-
-@app.route('/api/user_interests/<user_id>', methods=['GET'])
-def get_user_interests(user_id):
-    interests = user_interests_db.get(user_id, [])
-    return jsonify({"status": "success", "interests": interests})
-
 @app.route('/api/chat', methods=['POST'])
 def get_bot_response():
+    headers = request.headers
+    
+    isValid,session_id, response_message = verify_user(headers,app)
+    if(not isValid):
+        return response_message
+    
     question = request.json.get('question')
-    recordId = request.json.get('record_id')
-    database = get_DBconnection()
-    article_url = get_article_url(database,recordId)
-    response = response_retriever(article_url, question)
+    recordId = request.json.get('recordId')
 
+    article_url = get_article_url(db, recordId)
+    unique_id = session_id+recordId
+    print("article_url>>> ",article_url)
+    print("recordId>>>> ",recordId)
+    response = response_retriever(article_url, question,unique_id, bot_schema)
 
     print(response["answer"])
 
     return jsonify({"status": "success", "answer": response["answer"]})
 
+@app.route('/api/get_user_interests', methods=['GET'])
+def get_user_interests():
+    headers = request.headers
+    
+    isValid,clean_token, response_message = verify_user(headers,app)
 
+    if(isValid):
+        user_id = response_message
+        return find_user_interests(db,user_id)
+
+    else:
+        return response_message
+
+
+@app.route('/api/videos/<topic>', methods=['GET'])
+def get_videos(topic):
+    base_path = os.path.join(WATCH_DIR, topic)
+    if os.path.exists(base_path):
+        files = os.listdir(base_path)
+        videos = [f for f in files if f.endswith(('.mp4', '.mov'))]  # Add other video extensions if needed
+        return jsonify({'videos': videos})
+    else:
+        return jsonify({'error': 'Topic not found'}), 404
+
+@app.route('/api/video/<topic>/<filename>', methods=['GET'])
+def serve_video(topic, filename):
+    return send_from_directory(os.path.join(WATCH_DIR, topic), filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    finally:
+        observer.stop()
+        observer.join()
--- Frontend/flask-backend/retrievers/functions.py
+++ Frontend/flask-backend/retrievers/functions.py
@@ -1,33 +1,98 @@
 import re
+import bs4
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.memory import ChatMessageHistory
 from typing import Dict
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
 load_dotenv()
 
 
-def response_retriever(url, question):
+store = {}
+
+class schemaObject(object):
+    chat = ""
+    question_answering_prompt = ""
+    demo_ephemeral_chat_history = ""
+    contextualize_q_prompt = ""
+    def __init__(self,chat, question_answering_prompt,demo_ephemeral_chat_history, contextualize_q_prompt):
+        self.chat = chat
+        self.question_answering_prompt = question_answering_prompt
+        self.demo_ephemeral_chat_history = demo_ephemeral_chat_history
+        self.contextualize_q_prompt = contextualize_q_prompt
+
+def set_bot_schema():
+    chat = ChatOpenAI(model="gpt-3.5-turbo-1106")
+
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+        which might reference context in the chat history, formulate a standalone question \
+        which can be understood without the chat history. Do NOT answer the question, \
+        just reformulate it if needed and otherwise return it as is."""
+    
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    
+    #qa_prompt
+    question_answering_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Answer the user's questions based on the below context" 
+                "and start the response as 'As per the source ' or 'Well, ' or without and start phrase randomly." 
+                "If you do not know the answer, say 'Sorry, I do not see any reference for this in the source!':\n\n{context}",
+            ),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+
+    demo_ephemeral_chat_history = ChatMessageHistory()
+
+    bot_schema = schemaObject(chat, question_answering_prompt,demo_ephemeral_chat_history, contextualize_q_prompt)
+
+    return bot_schema
+
+
+def response_retriever(url, question,session_id, bot_schema):
     if validate_url(url):
         data = load_data_from_web(url)
         vectorstore = doc_splitter(data)
         retriever = vectorstore.as_retriever(k=4)
-        retreived_docs = doc_retriever(question,retriever)
-        retrieval_chain, demo_ephemeral_chat_history = get_retreiver_chain(retreived_docs,retriever,question)
-        response = invoke_chain(retrieval_chain,demo_ephemeral_chat_history)
-
+        history_aware_retriever = create_history_aware_retriever(
+            bot_schema.chat, retriever, bot_schema.contextualize_q_prompt
+        )
+        retrieval_chain = get_retreiver_chain(history_aware_retriever, bot_schema.chat, bot_schema.question_answering_prompt)
+        conversational_rag_chain = get_conversational_rag_chain(retrieval_chain)
+        response = invoke_conversational_chat(conversational_rag_chain,question,session_id)
         return response
     else:
         return "Invalid news source"
     
 def load_data_from_web(url):
-    loader =  WebBaseLoader(url) 
+    ### Construct retriever ###
+    loader = WebBaseLoader(
+        web_paths=(url,),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
     data = loader.load()
     return data
 
@@ -37,53 +102,40 @@ def doc_splitter(data):
     vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
     return vectorstore
 
-def doc_retriever(question,retriever):
-    retreived_docs = retriever.invoke(question)
-    return retreived_docs
+def get_retreiver_chain(history_aware_retriever, chat, question_answering_prompt):
+    print('im here')
 
-def get_retreiver_chain(docs,retriever,question):
-    chat = ChatOpenAI(model="gpt-3.5-turbo-1106")
+    question_answer_chain = create_stuff_documents_chain(chat, question_answering_prompt)
 
-        #prompt
-    question_answering_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            "Answer the user's questions based on the below context" 
-            "and start the response as 'As per the article'." 
-            "If you do not know the answer, say 'Sorry, I do not see any reference for this in the article!':\n\n{context}",
-        ),
-        MessagesPlaceholder(variable_name="messages"),
-    ]
-)
-    document_chain = create_stuff_documents_chain(chat, question_answering_prompt)
+    retrieval_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    return retrieval_chain
 
-        #invoke chain
-    demo_ephemeral_chat_history = ChatMessageHistory()
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    
+    print(store)
+    return store[session_id]
 
-    demo_ephemeral_chat_history.add_user_message(question)
+def get_conversational_rag_chain(rag_chain):
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+    return conversational_rag_chain
 
-    document_chain.invoke(
-    {
-        "messages": demo_ephemeral_chat_history.messages,
-        "context": docs,
-    }
-)
-    retrieval_chain = RunnablePassthrough.assign(
-    context=parse_retriever_input | retriever,
-).assign(
-    answer=document_chain,
-)
-    return retrieval_chain, demo_ephemeral_chat_history
-
-def invoke_chain(chain,chat_history):
-     response = chain.invoke(
-    {
-        "messages": chat_history.messages,
-    }
-)
-     return response
-
+def invoke_conversational_chat(conversational_rag_chain,question,session_id):
+    response = conversational_rag_chain.invoke(
+    {"input": question},
+    config={
+        "configurable": {"session_id": session_id}
+    },
+    )
+    return response
 
 def validate_url(url):
     regex = re.compile(
